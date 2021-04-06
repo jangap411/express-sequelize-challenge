fe79ebaa26dafca2158a33e5baafd9820091925f
jangap411 <JedidiahANGAP@switchmaven.com>
2021-04-06 21:35:11 +1000

    updates get.js

--- routes/get.js
+++ routes/get.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const app = express();
 const User = require("../models/User");
 
+let data = {
+  message: "",
+};
+
 //middleware
 app.use(express.json());
 
@@ -15,6 +19,23 @@ router.get("/", (req, res) => {
 router.get("/user", async (req, res) => {
   const users = await User.findAll();
   res.send(users);
+});
+
+//members only
+router.get("/supercoolmembersonlypage", (req, res) => {
+  res.render("pages/members");
+});
+
+//logout
+router.get("/logout", (req, res) => {
+  let id = req.cookies.SID;
+
+  //creates the cookie that holds the UUID (the session ID)
+  res.cookie("SID", id, {
+    expires: new Date(Date.now() - 200),
+    httpOnly: true,
+  });
+  res.render("pages/index");
 });
 
 module.exports = router;
